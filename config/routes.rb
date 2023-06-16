acfa5fcffae68f11d7fd56f841ad03fe4310abfd
Ruperto <ru.bugueno@gmail.com>
2023-06-16 00:44:01 -0400

    Theme lens

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  get 'pages/musica'
   get 'home/index'
-  get 'home/musica'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+   root "home#index"
 end
